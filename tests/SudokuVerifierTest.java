b69be43bc39a496839940e17430d902bc4619d09
Aleksi Värttö <alkeksi93@gmail.com>
2023-11-22 21:15:32 +0200

    added tests

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -10,7 +10,16 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 String c = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 String i = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+String sht = "1";
+String shtMid = "4173698256321589479587243168254371697915864";
+String sTooLong = "4173698256321589479587243168254371697915864323469127582896435715732916841648752935293";
+String wayTooShort = "";
+String ofCharacters = "aBcDeFgHiJkLmNoPqRsTuVwXyZaBcDeFgHiJkLmNoPqRsTuVwXyZaBcDeFgHiJkLmN";
+String ofSpecials = "!@#$%^&*()_+-=[]{}|;:'\\\",.<>?/`!@#$%^&*()_+-=[]{}|\\\\;:'\\\",.<>?/~`!@#$%^&*()_+-=[]";
+String ofSpace = "                                                                               ";
+
 SudokuVerifier v = new SudokuVerifier();
+
 
 	@Test
 	public void testCorrectString() {
@@ -24,6 +33,45 @@ SudokuVerifier v = new SudokuVerifier();
 		assertEquals("incorrect string", a, -2);
 		
 	}
+	
+	@Test
+	public void test1(){
+		int a = v.verify(sht);
+		assertEquals(-1, a);
+	}
+	@Test
+	public void test2() {
+		int a = v.verify(shtMid);
+		assertEquals(-1, a);
+	}
+	@Test
+	public void test3() {
+		int a = v.verify(sTooLong);
+		assertEquals(-1, a);
+	}
+	@Test
+	public void test4() {
+		int a = v.verify(wayTooShort);
+		assertEquals(-1, a);
+	}
+	@Test
+	public void test5() {
+		int a = v.verify(ofCharacters);
+		assertEquals(-1, a);
+	}
+	@Test
+	public void test6() {
+		int a = v.verify(ofSpecials);
+		assertEquals(1, a);
+	}
+	@Test
+	public void test7() {
+		int a = v.verify(ofSpace);
+		assertEquals(-1, a);
+	}
+	
+	
+	
 	
 	
 }
